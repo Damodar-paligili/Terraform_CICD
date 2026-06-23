e65262ee1584fdd8bcd5c293006118af3724bc7f
Damodar-paligili <damodarpp99@gmail.com>
2026-06-23 12:45:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
+    ami = "ami-08f44e8eca9095668"
+    instance_type = "t3.micro"
     tags = {
-      Name = "nitnit"
+      Name = "damodar"
     }
 }
